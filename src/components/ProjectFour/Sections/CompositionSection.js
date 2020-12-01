8c804715bf13cb545f012107fad308544b7eda7c
krish <krish@buildable.io>
2020-12-02 03:09:03 +0530

    Image exp

--- src/components/ProjectFour/Sections/CompositionSection.js
+++ src/components/ProjectFour/Sections/CompositionSection.js
@@ -17,40 +17,40 @@ const boxStyle = (isTabletOrMobileDevice) => {
 
 const images = [
     {
-        image: "/images/1.png"
+        image: "/images/1.jpg"
     },
     {
-        image: "/images/2.png"
+        image: "/images/2.jpg"
     },
     {
-        image: "/images/3.png"
+        image: "/images/3.jpg"
     },
     {
-        image: "/images/4.png"
+        image: "/images/4.jpg"
     },
     {
-        image: "/images/5.png"
+        image: "/images/5.jpg"
     },
     {
-        image: "/images/6.png"
+        image: "/images/6.jpg"
     },
     {
-        image: "/images/7.png"
+        image: "/images/7.jpg"
     },
     {
-        image: "/images/8.png"
+        image: "/images/8.jpg"
     },
     {
-        image: "/images/9.png"
+        image: "/images/9.jpg"
     },
     {
-        image: "/images/10.png"
+        image: "/images/10.jpg"
     },
     {
-        image: "/images/11.png"
+        image: "/images/11.jpg"
     },
     {
-        image: "/images/12.png"
+        image: "/images/12.jpg"
     }
 ]
 
